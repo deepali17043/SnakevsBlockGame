a9c3f82e4913c746739337e80d856026a2962a16
deepali17043 <44729497+deepali17043@users.noreply.github.com>
2018-12-03 01:48:38 +0530

    Update Shield.java

--- Shield.java
+++ Shield.java
@@ -1,19 +1,36 @@
 package application;
 
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class Shield extends Tokens {
 	
-	private int time;
-	
-	@Override
-	public int getval() {
-		return time;
-		
-	}
+	public static int time = 5;
+	//shield remains with the snake for five seconds
 
+	public Shield(boolean set, PlayGame g) {
+		super(set, g);
+	}
+	
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
+		if(!getvisibility())
+			return;
+		setvisibility(false);
+		//GamePlay.sheildsnake();
+	}
+
+	@Override
+	public StackPane getToken() {
+		Circle c = new Circle();
+		c.setRadius(10);
+		c.setFill(Color.DARKTURQUOISE);
 		
+		addToPane(c);
+		
+		return getPane();
 	}
 
 }
