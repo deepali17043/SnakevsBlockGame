4cbf9bd10fdd6aa892bab4bc350f914d849102fd
deepali17043 <44729497+deepali17043@users.noreply.github.com>
2018-12-03 01:50:39 +0530

    Update Magnet.java

--- Magnet.java
+++ Magnet.java
@@ -1,18 +1,33 @@
 package application;
 
-public class Magnet extends Tokens {
-	
-	private int radius;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
+public class Magnet extends Tokens {
+	//collects all the balls in the same line as magnet.
+
+	public Magnet(boolean set, PlayGame g) {
+		super(set, g);
+	}
+	
 	@Override
-	public int getval() {
-		return radius;
+	public void destroy() {
+		if(!getvisibility()) 
+			return;
+		setvisibility(false);
+		//GamePlay.collectcoins();
 	}
 
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
+	public StackPane getToken() {
+		Circle c = new Circle();
+		c.setRadius(10);
+		c.setFill(Color.DARKORCHID);
 		
+		addToPane(c);
+		
+		return getPane();
 	}
 	
 }
