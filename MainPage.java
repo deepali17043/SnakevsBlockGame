0f3e27846d4f6372ef7fc93ee65a153ae181a43a
divya701 <44730583+divya701@users.noreply.github.com>
2018-12-03 01:01:09 +0530

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -19,7 +20,19 @@ public class MainPage extends Application{
 	
 	private Button startgame, resumegame, help, leaderboard;
 	private Button Exit;
+	private playgame1 gameplay;
+	private LeaderBoard LeaderBoardPage;
+	private Help HelpPage;
 	private boolean unfinishedgame;
+	
+	public MainPage() {
+		gameplay = new playgame1();
+		LeaderBoardPage = new LeaderBoard();
+		HelpPage = new Help();
+		//recordedgame = deserialize();
+	}
+	
+	//add methods serialize and deserialize
 	
 	public static void main(String[] args) {		
 		launch(args);
@@ -57,11 +70,46 @@ public class MainPage extends Application{
 		}
 		HBox h2 = new HBox(leaderboard);
 		HBox h3 = new HBox(help);
+		HBox h4 = new HBox(Exit);
 		
 		h0.setAlignment(Pos.CENTER);
 		h1.setAlignment(Pos.CENTER);
 		h2.setAlignment(Pos.CENTER);
 		h3.setAlignment(Pos.CENTER);
+		h4.setAlignment(Pos.CENTER);
+		
+		startgame.setOnAction(e -> {
+			try {
+				gameplay.start(primaryStage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		/*resumegame.setOnAction(e -> {
+			recordedgame.start(primaryStage);
+		});*/
+		
+		leaderboard.setOnAction(e -> {
+			try {
+				LeaderBoardPage.start(primaryStage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		help.setOnAction(e -> {
+			try {
+				HelpPage.start(primaryStage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		Exit.setOnAction(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
 		
 		VBox V = new VBox(h0, h1, h2, h3);
 		V.setPadding(new Insets(10, 20, 10, 20));
@@ -73,6 +121,10 @@ public class MainPage extends Application{
 		Scene scene = new Scene(V, 300, 500);
 		scene.setFill(Color.BLACK);
 		primaryStage.setScene(scene);
+		primaryStage.setOnCloseRequest(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
 		primaryStage.show();
 	}
 
