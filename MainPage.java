283b54b88f97a588527dfb6d2e79faed75d62d91
deepali17043 <44729497+deepali17043@users.noreply.github.com>
2018-12-03 01:49:34 +0530

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Date;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -23,11 +24,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+
 public class MainPage extends Application{
 	
 	private Button startgame, resumegame, help, leaderboard;
 	private Button Exit;
-	private PlayGame gameplay;
+	private PlayGame gameplay, recordedgame;
 	private LeaderBoard LeaderBoardPage;
 	private Help HelpPage;
 	private boolean unfinishedgame;
@@ -35,10 +37,17 @@ public class MainPage extends Application{
 	static class PlayGameSerialise implements Serializable {
 		private int score;
 		private int snakelength;
+		private boolean unfinishedgame;
 		
 		PlayGameSerialise(PlayGame g) {
 			score = g.getscore();
 			snakelength = g.getSnakeLength();
+			unfinishedgame = true;
+		}
+		
+		PlayGameSerialise(){
+			unfinishedgame = false;
+			score = snakelength = 0;
 		}
 		
 		int getscore() {
@@ -48,13 +57,17 @@ public class MainPage extends Application{
 		int getSnakeLength() {
 			return snakelength;
 		}
+		
+		boolean getunfinishedgame() {
+			return unfinishedgame;
+		}
 	}
 	
-	public MainPage() {
+	public MainPage() throws ClassNotFoundException, IOException {
 		gameplay = new PlayGame(this);
 		LeaderBoardPage = new LeaderBoard(this);
 		HelpPage = new Help(this);
-		//recordedgame = deserialize();
+		//deserialize();
 	}
 	
 	//add methods serialize and deserialize
@@ -63,8 +76,16 @@ public class MainPage extends Application{
 		launch(args);
 	}
 	
-	public void serialize() throws IOException {
-		PlayGameSerialise P = new PlayGameSerialise(gameplay);
+	public void AddNewScore(int x) {
+		LeaderBoardPage.addnewscore(x, new Date());
+	}
+	
+	public void serialize(boolean k) throws IOException {
+		PlayGameSerialise P;
+		if(k)
+			P = new PlayGameSerialise(gameplay);
+		else
+			P = new PlayGameSerialise();
 		
 		ObjectOutputStream out = null;
 		try {
@@ -76,7 +97,7 @@ public class MainPage extends Application{
 		}
 	}
 	
-	public PlayGame deserialize() throws IOException, ClassNotFoundException {
+	public void deserialize() throws IOException, ClassNotFoundException {
 			PlayGameSerialise P;
 			
 		    ObjectInputStream in = null;
@@ -86,12 +107,30 @@ public class MainPage extends Application{
 		    } finally {
 		    		in.close();
 		    }
-		    PlayGame g = new PlayGame(P.getSnakeLength(), P.getscore(), this);
-		    return g;
+		    if(P.unfinishedgame) {
+		    		recordedgame = new PlayGame(P.getSnakeLength(), P.getscore(), this);
+		    		resumegame.setVisible(true);
+		    }
 	}
 	
-	public void setunfinishedgame(boolean k) {
-		unfinishedgame = k;
+	public void serializeLeaderBoard() throws IOException {
+		ObjectOutputStream out = null;
+		try {
+			out = new ObjectOutputStream(new FileOutputStream("HighScores.txt"));
+			out.writeObject(LeaderBoardPage);
+		} finally {
+			out.close();
+		}
+	}
+	
+	public void deserializeLeaderBoard() throws IOException, ClassNotFoundException {
+		ObjectInputStream in = null;
+		try {
+			in = new ObjectInputStream(new FileInputStream("HighScores.txt"));
+			LeaderBoardPage = (LeaderBoard) in.readObject();
+		} finally {
+			in.close();
+		}
 	}
 
 	@Override
@@ -105,20 +144,28 @@ public class MainPage extends Application{
 		leaderboard = new Button("Leader Board");
 		Exit = new Button("Close Application");
 		
-		startgame.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE");
-		help.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE");
-		leaderboard.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE");
-		Exit.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE");
+		startgame.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE;");
+		help.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE;");
+		leaderboard.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE;");
+		Exit.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE;");
 		
 		Label l = new Label("Snake Vs. Block\nGame");
 		l.setTextAlignment(TextAlignment.CENTER);
-		l.setStyle("-fx-font: 30 arial; -fx-text-fill: WHITE;");
+		l.setStyle("-fx-font: 35 arial; -fx-text-fill: WHITE;");
 		HBox h0 = new HBox(l);
 		HBox h1 = new HBox(startgame);
 		if(unfinishedgame) {
 			resumegame = new Button("Resume");
 			h1.getChildren().add(resumegame);
 			h1.setSpacing(50);
+			resumegame.setStyle("-fx-font: 12 arial; -fx-base:#4c3ee0; -fx-text-fill: WHITE;");
+			resumegame.setOnAction(e -> {
+				try {
+					recordedgame.start(primaryStage);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			});
 		}
 		HBox h2 = new HBox(leaderboard);
 		HBox h3 = new HBox(help);
@@ -137,10 +184,6 @@ public class MainPage extends Application{
 				e1.printStackTrace();
 			}
 		});
-		
-		/*resumegame.setOnAction(e -> {
-			recordedgame.start(primaryStage);
-		});*/
 		
 		leaderboard.setOnAction(e -> {
 			try {
