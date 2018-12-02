30e644234de7b52c880b60eb0f3a8e21197d5cb2
deepali17043 <44729497+deepali17043@users.noreply.github.com>
2018-12-03 01:53:08 +0530

    Update DestroyBlocks.java

--- DestroyBlocks.java
+++ DestroyBlocks.java
@@ -7,7 +7,40 @@ public class DestroyBlocks extends Tokens {
 	@Override
 	public int getval() {
 		return radius;
+	}package application;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class DestroyBlocks extends Tokens {
+	
+	public DestroyBlocks(boolean set, PlayGame g) {
+		super(set,g);
 	}
+
+	@Override
+	public StackPane getToken() {
+		
+		Circle c = new Circle();
+		c.setRadius(10);
+		c.setFill(Color.RED);
+		
+		addToPane(c);
+		
+		return getPane();
+	}
+
+	@Override
+	public void destroy() {
+		if(!getvisibility())
+			return;
+		setvisibility(false);
+		//GamePlay.resetblocks();
+	}
+
+}
+
 
 	@Override
 	public void destroy() {
