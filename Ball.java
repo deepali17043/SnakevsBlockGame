860b38738a9a1dad92b1da4312c815e6874c76ec
deepali17043 <44729497+deepali17043@users.noreply.github.com>
2018-12-03 01:59:33 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,18 +1,46 @@
 package application;
 
-public class Ball extends Tokens{
+import java.util.Random;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+public class Ball extends Tokens {
 	
 	private int value;
 	
-	@Override
-	public int getval() {
+	public Ball(boolean set, PlayGame g) {
+		super(set, g);
+		Random r = new Random();
+		value = r.nextInt(10) + 1;
+	}
+	
+	public int getvalue() {
 		return value;
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
+		if(!getvisibility())
+			return;
+		setvisibility(false);
+		//GamePlay.incsnakelength(value);
+	}
+
+	@Override
+	public StackPane getToken() {
+		Circle c = new Circle();
+		c.setRadius(10);
+		c.setFill(Color.YELLOW);
 		
+		Text t = new Text();
+		t.setText("" + value);
+		
+		addToPane(c, t);
+		
+		return getPane();
 	}
 
 }
